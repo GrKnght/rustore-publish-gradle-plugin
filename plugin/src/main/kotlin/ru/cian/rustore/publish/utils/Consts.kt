4f686103a985954da4546963e80bcbf66a7cfaea
R.Faskhetdinov <rrfaskhetdinov@detmir.ru>
2024-06-28 18:24:24 +0300

    Delayed publication logic

--- plugin/src/main/kotlin/ru/cian/rustore/publish/utils/Consts.kt
+++ plugin/src/main/kotlin/ru/cian/rustore/publish/utils/Consts.kt
@@ -3,7 +3,7 @@ package ru.cian.rustore.publish.utils
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-const val RELEASE_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZZ"
+const val RELEASE_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX"//"yyyy-MM-dd'T'HH:mm:ssZZ"
 
 const val ONE_HOUR_IN_MILLIS: Long = 1000 * 60 * 60
 
